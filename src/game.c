7fd343f3a1f511054294d2211f3cabb0e3f34ac6
Carl Riis <carl.aagot.riis@gmail.com>
2022-10-23 11:28:55 +0200

    Well working flipping

--- src/game.c
+++ src/game.c
@@ -5,55 +5,29 @@
 
 const unsigned long cubeMemSize = CUBE_POINTS_N * sizeof(Point);
 
-const float MAX_DEPTH = 100;
-
-float FAR_BOUNDS_X = 15;
-float NEAR_BOUNDS_X = 6;
-
-const float FAR_BOUNDS_Y = 15;
-const float NEAR_BOUNDS_Y = 6;
-
-const float X_CUBE_MAX = 6;
-const float Y_CUBE_MAX = 6;
-
-float xBounds(float z) {
-  float xRatio = NEAR_BOUNDS_X / FAR_BOUNDS_X;
-  return z * xRatio;
-}
-
-float yBounds(float z) {
-  float yRatio = NEAR_BOUNDS_Y / FAR_BOUNDS_Y;
-  return z * yRatio;
-}
+const float MAX_DEPTH = 200;
+const float BOUNDS_X = 15;
+const float BOUNDS_Y = 15;
 
 float rr(float min, float max) {
   return min + (float)rand() / ((float)RAND_MAX / (max - min));
 }
 
 void addNewCube(Cube cubes[], int *cubesLength) {
-  float z = MAX_DEPTH;
-
   Point p = {
-      .x = rr(-xBounds(z), xBounds(z)),
-      .y = rr(-yBounds(z), yBounds(z)),
-      .z = z,
+      .x = rr(-BOUNDS_X, BOUNDS_X),
+      .y = rr(-BOUNDS_Y, BOUNDS_Y),
+      .z = MAX_DEPTH,
   };
-
   Cube cube = newCube(p, 0.5);
   cubes[(*cubesLength)++] = cube;
 }
 
 void addInitialCube(Cube cubes[], int *cubesLength) {
-  float x, y, z;
-  do {
-    x = rr(-FAR_BOUNDS_X, FAR_BOUNDS_X);
-    y = rr(-FAR_BOUNDS_Y, FAR_BOUNDS_Y);
-    z = rr(0, MAX_DEPTH);
-  } while (fabs(x) > xBounds(z) || fabs(y) > yBounds(z));
   Point p = {
-      .x = x,
-      .y = y,
-      .z = z,
+      .x = rr(-BOUNDS_X, BOUNDS_X),
+      .y = rr(-BOUNDS_Y, BOUNDS_Y),
+      .z = rr(0, MAX_DEPTH),
   };
   Cube cube = newCube(p, 0.5);
   cubes[(*cubesLength)++] = cube;
@@ -77,66 +51,25 @@ void rearrangeCubesToTakeOutRemoved(Cube cubes[], int *cubesLength, int removedN
   }
 }
 
-// void loopBackOutOfBoundsCubes(Cube cubes[], int *cubesLength, int *cubesRemoved) {
-//   for (int i = 0; i < *cubesLength; i++) {
-//     float cXBounds = xBounds(cubes[i][0].z);
-//     if (cubes[i][0].x > cXBounds) {
-//       // for (int p = 0; p < 20; p++) {
-//       //   cubes[i][p].x -= cXBounds * 2;
-//       // }
-//       removeCube(cubes, i);
-//       *cubesRemoved += 1;
-//     } else if (cubes[i][0].x < -cXBounds) {
-//       removeCube(cubes, i);
-//       *cubesRemoved += 1;
-//       // for (int p = 0; p < 20; p++) {
-//       //   cubes[i][p].x += cXBounds * 2;
-//       // }
-//     }
-//   }
-// }
-
-int removeOrFlipCubeIfOutOfBounds(Cube cubes[], int i, float xDiff, float yDiff, float zDiff) {
-  float cXBounds = xBounds(cubes[i][0].z + zDiff);
-  float cYBounds = yBounds(cubes[i][0].z + zDiff);
-
-  // If out-of-bounds rn, just remove, this wasn't caused by movement
-  // if (xDiff > 0)
-  //  aka cubes moved to the right (pressing a)
-  //  aka some cubes on the right might become out of bounds *because* we moved
-  //      if That happens we should flip them
-
-  if (xDiff == 0) {
-    if (cubes[i][0].x > cXBounds || cubes[i][2].x < -cXBounds) {
-      removeCube(cubes, i);
-      return 1;
+void flipCubeIfOutOfBounds(Cube cubes[], int i) {
+  if (cubes[i][0].x < -BOUNDS_X) {
+    for (int p = 0; p < 20; p++) {
+      cubes[i][p].x += BOUNDS_X * 2;
     }
-  } else {
-    if ((cubes[i][0].x + xDiff) > cXBounds || (cubes[i][2].x + xDiff) < -cXBounds) {
-      float mod = xDiff > 0 ? -1 : 1;
-      for (int p = 0; p < 20; p++) {
-        cubes[i][p].x += cXBounds * 2 * mod;
-      }
-      return 0;
+  } else if (cubes[i][0].x > BOUNDS_X) {
+    for (int p = 0; p < 20; p++) {
+      cubes[i][p].x -= BOUNDS_X * 2;
     }
   }
-
-  if (yDiff == 0) {
-    if (cubes[i][0].y > cYBounds || cubes[i][2].y < -cYBounds) {
-      removeCube(cubes, i);
-      return 1;
+  if (cubes[i][0].y < -BOUNDS_Y) {
+    for (int p = 0; p < 20; p++) {
+      cubes[i][p].y += BOUNDS_X * 2;
     }
-  } else {
-    if ((cubes[i][0].y + yDiff) > cYBounds || (cubes[i][2].y + yDiff) < -cYBounds) {
-      float mod = yDiff > 0 ? -1 : 1;
-      for (int p = 0; p < 20; p++) {
-        cubes[i][p].y += cYBounds * 2 * mod;
-      }
-      return 0;
+  } else if (cubes[i][0].y > BOUNDS_Y) {
+    for (int p = 0; p < 20; p++) {
+      cubes[i][p].y -= BOUNDS_Y * 2;
     }
   }
-
-  return 0;
 }
 
 int compareSize(const void *a, const void *b) {
@@ -146,34 +79,17 @@ int compareSize(const void *a, const void *b) {
   return (cube1[0].z < cube2[0].z) - (cube1[0].z > cube2[0].z);
 }
 
-float timeSinceLastCube = 0;
-int cubesRemovedForDistance = 0;
-
 void gameFrame(SDL_Event e, float deltaTime, Cube cubes[], int *cubesLength) {
   float speed = 70 * deltaTime;
   float moveSpeed = 30 * deltaTime;
   if (*cubesLength == 0) {
-    while ((*cubesLength) < 300) {
+    while ((*cubesLength) < 500) {
       addInitialCube(cubes, cubesLength);
     }
   } else {
-    // printf("\nDistance %f %f", cubes[*cubesLength - 1][0].z, cubes[0][0].z);
-
-    // if (cubes[0][0].z < MAX_DEPTH * 0.999) {
-    //   for (int i = 0; i < 2; i++) {
-    //     addNewCube(cubes, cubesLength);
-    //   }
-    // }
-
-    for (int i = 0; i < cubesRemovedForDistance + 1; i++) {
+    while ((*cubesLength) < 500) {
       addNewCube(cubes, cubesLength);
     }
-    cubesRemovedForDistance = 0;
-
-    // timeSinceLastCube += deltaTime;
-    // printf("\nTime %f", timeSinceLastCube);
-    // if (timeSinceLastCube > 0.01) {
-    // }
   }
 
   int cubesRemoved = 0;
@@ -203,18 +119,13 @@ void gameFrame(SDL_Event e, float deltaTime, Cube cubes[], int *cubesLength) {
     if (cubes[i][0].z < 1.5) {
       removeCube(cubes, i);
       cubesRemoved += 1;
-      cubesRemovedForDistance += 1;
     } else {
-      int removed = removeOrFlipCubeIfOutOfBounds(cubes, i, xDiff, yDiff, zSpeed);
-      if (removed) {
-        cubesRemoved += 1;
-      } else {
-        for (int p = 0; p < 20; p++) {
-          cubes[i][p].x += xDiff;
-          cubes[i][p].y += yDiff;
+      flipCubeIfOutOfBounds(cubes, i);
+      for (int p = 0; p < 20; p++) {
+        cubes[i][p].x += xDiff;
+        cubes[i][p].y += yDiff;
 
-          cubes[i][p].z += zSpeed;
-        }
+        cubes[i][p].z += zSpeed;
       }
     }
   }
